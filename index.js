8c37ec06c163c563199cae7568bcd5f69546b478
Ghulam Rauf Khan <159796130+GRAUF@users.noreply.github.com>
2024-03-23 22:04:03 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// COMPUTER WILL GENERATE RANDOM NUMBER BETWEEN 1 AND 10
+// USER WILL GUESS THE NUMBER
+// COMPARE USER INPUT WITH COMPUTER INPUT NUMBER AND SHOW RESULTS.
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "guessNumber",
+        message: "please Entre Guess number between 1 and 10,$$$$",
+    }
+]);
+if (answers.guessNumber === randomNumber) {
+    console.log("Congrats! You Won! $$$");
+}
+else {
+    console.log("You Lost! try again later");
+}
